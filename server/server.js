254943c62242d0bf847a4c5561820c47149d93e3
shiv <shivbichve749@gmail.com>
2025-10-08 10:52:37 +0530

    removed / from cors origin

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 await connectCloudinary();
 
 app.use(cors({
-    origin: "https://quick-ai-brown-gamma.vercel.app/",
+    origin: "https://quick-ai-brown-gamma.vercel.app",
     credentials: true
 }));
 app.use(express.json());
